bf2d3487a12529f079e70d63d4b0e612a9c8b4c3
Andrew Grischenko <andrew.grischenko@gmail.com>
2022-09-23 22:24:29 +1000

    Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit;
 
 streamlit.title('Healthy dinner!')
+
+streamlit.header('Breakfast Favourites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
